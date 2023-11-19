b8571a884e79d43e37c2672911d8c3ef97fe00fd
Some-Regular-Person <31082683+Justin-J-K@users.noreply.github.com>
2023-11-19 12:37:26 -0800

    Add initial functions

--- app/src/main/java/cpp/cs4750/rssfeedreader/repository/FeedRepository.kt
+++ app/src/main/java/cpp/cs4750/rssfeedreader/repository/FeedRepository.kt
@@ -1,4 +1,15 @@
 package cpp.cs4750.rssfeedreader.repository
 
+import cpp.cs4750.rssfeedreader.model.Feed
+import cpp.cs4750.rssfeedreader.model.Item
+import kotlinx.coroutines.flow.Flow
+
 class FeedRepository {
+
+    suspend fun fetchItems(): List<Item> = TODO("Fetch items from all feed urls and add to database")
+
+    fun getItems(): Flow<List<Item>> = TODO("Get items from database")
+
+    fun getFeeds(): Flow<List<Feed>> = TODO("Get feeds from database")
+
 }
--- app/src/main/java/cpp/cs4750/rssfeedreader/repository/PreferencesRepository.kt
+++ app/src/main/java/cpp/cs4750/rssfeedreader/repository/PreferencesRepository.kt
@@ -1,4 +1,7 @@
 package cpp.cs4750.rssfeedreader.repository
 
 class PreferencesRepository {
+    suspend fun setPolling(isPolling: Boolean) {
+        TODO()
+    }
 }
