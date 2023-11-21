0ddcee73146dcf0f193156a9100bdae735d227c0
Christian Williams <ciwilliams@cpp.edu>
2023-11-20 21:40:47 -0800

    Created notification channel

--- app/src/main/java/cpp/cs4750/rssfeedreader/FeedReaderApplication.kt
+++ app/src/main/java/cpp/cs4750/rssfeedreader/FeedReaderApplication.kt
@@ -1,4 +1,34 @@
 package cpp.cs4750.rssfeedreader
 
-class FeedReaderApplication {
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import android.content.Context
+import android.provider.Settings.Global.getString
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
+
+
+const val NOTIFICATION_CHANNEL_ID = "rss_poll"
+class FeedReaderApplication : Application() {
+
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+
+    fun notif() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.notification_channel_name)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel =
+                   NotificationChannel (NOTIFICATION_CHANNEL_ID, name, importance)
+            val notificationManager: NotificationManager? =
+                getSystemService(NotificationManager::class.java)
+            notificationManager?.createNotificationChannel(channel)
+        }
+
+    }
 }
