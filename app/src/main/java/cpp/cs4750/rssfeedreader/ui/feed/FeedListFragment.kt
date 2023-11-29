92c9fa0abe5b1750c780c4ae049992c0d26e7fda
whatsGee <gadejesus09@gmail.com>
2023-11-28 16:47:34 -0800

    WIP FeedListFragment
    making feed list to work

--- app/src/main/java/cpp/cs4750/rssfeedreader/ui/feed/FeedListFragment.kt
+++ app/src/main/java/cpp/cs4750/rssfeedreader/ui/feed/FeedListFragment.kt
@@ -4,28 +4,61 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
 import androidx.fragment.app.Fragment
-import cpp.cs4750.rssfeedreader.databinding.FragmentFeedListBinding
+import cpp.cs4750.rssfeedreader.R
 
 class FeedListFragment : Fragment() {
 
-    private var _binding: FragmentFeedListBinding? = null
-    private val binding
-        get() = checkNotNull(_binding) {
-            "Cannot access binding because it is null. Is the view visible?"
-        }
+    // Initialize the list of URLs
+    private val urlList = ArrayList<String>()
+
+    // Declare UI elements
+    private lateinit var urlEditText: EditText
+    private lateinit var addButton: Button
+    private lateinit var feedListView: ListView
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentFeedListBinding.inflate(inflater, container, false)
-        return binding.root
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_feed_list, container, false)
+
+        // Initialize UI elements
+        urlEditText = view.findViewById(R.id.urlEditText)
+        addButton = view.findViewById(R.id.addButton)
+        feedListView = view.findViewById(R.id.feedList)
+
+        // Initialize the ArrayAdapter for the ListView
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, urlList)
+        feedListView.adapter = adapter
+
+        // Set click listener for the Add button
+        addButton.setOnClickListener {
+            addUrlToList()
+        }
+
+        return view
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    // Function to add a new URL to the list
+    private fun addUrlToList() {
+        val newUrl = urlEditText.text.toString().trim()
+
+        // Check if the URL is not empty
+        if (newUrl.isNotEmpty()) {
+            // Add the URL to the list
+            urlList.add(newUrl)
+
+            // Notify the adapter that the data set has changed
+            (feedListView.adapter as ArrayAdapter<*>).notifyDataSetChanged()
+
+            // Clear the EditText
+            urlEditText.text.clear()
+        }
     }
 }
