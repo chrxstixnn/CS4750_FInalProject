8e30720cdb346636d918204ec5890695766e6b15
Christian Williams <ciwilliams@cpp.edu>
2023-12-03 23:18:41 -0800

    Initialized repository

--- app/src/main/java/cpp/cs4750/rssfeedreader/FeedReaderApplication.kt
+++ app/src/main/java/cpp/cs4750/rssfeedreader/FeedReaderApplication.kt
@@ -1,4 +1,36 @@
 package cpp.cs4750.rssfeedreader
 
-class FeedReaderApplication {
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import android.content.Context
+import android.provider.Settings.Global.getString
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
+import cpp.cs4750.rssfeedreader.repository.FeedRepository
+
+
+const val NOTIFICATION_CHANNEL_ID = "rss_poll"
+class FeedReaderApplication : Application() {
+
+
+    override fun onCreate() {
+        super.onCreate()
+        FeedRepository.initialize(this)
+    }
+
+
+    fun notif() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.notification_channel_name)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel =
+                NotificationChannel (NOTIFICATION_CHANNEL_ID, name, importance)
+            val notificationManager: NotificationManager? =
+                getSystemService(NotificationManager::class.java)
+            notificationManager?.createNotificationChannel(channel)
+        }
+
+    }
 }
