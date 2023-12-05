47bad75ab02dea46e29083e4f52236dc46893f20
Christian Williams <ciwilliams@cpp.edu>
2023-12-04 23:25:31 -0800

    Completed PreferencesRepository

--- app/src/main/java/cpp/cs4750/rssfeedreader/repository/PreferencesRepository.kt
+++ app/src/main/java/cpp/cs4750/rssfeedreader/repository/PreferencesRepository.kt
@@ -1,7 +1,90 @@
 package cpp.cs4750.rssfeedreader.repository
 
-class PreferencesRepository {
-    suspend fun setPolling(isPolling: Boolean) {
-        TODO()
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStoreFile
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
+
+
+class PreferencesRepository private constructor(
+    private val dataStore: DataStore<Preferences>
+){
+    val storedQuery: Flow<String> = dataStore.data.map {
+        it[ACCESS_QUERY] ?: ""
+    }.distinctUntilChanged()
+
+    suspend fun setStoredQuery(query: String){
+        dataStore.edit{
+            it[ACCESS_QUERY] = query
+        }
     }
+
+    val lastResultId: Flow<String> = dataStore.data.map{
+        it[LAST_ARTICLE] ?: ""
+    }.distinctUntilChanged()
+
+    suspend fun setLastResultId(lastResultId: String){
+        dataStore.edit{
+            it[LAST_ARTICLE] = lastResultId
+        }
+    }
+
+    val isPolling: Flow<Boolean> = dataStore.data.map{
+        it[POLLING] ?: false
+    }.distinctUntilChanged()
+
+    suspend fun setPolling(isPolling: Boolean){
+        dataStore.edit {
+            it[POLLING] = isPolling
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+    companion object {
+
+        private val ACCESS_QUERY = stringPreferencesKey("query_key")
+        private val LAST_ARTICLE = stringPreferencesKey("lastArticleId")
+        private val POLLING = booleanPreferencesKey("polling")
+        private var INSTANCE: PreferencesRepository? = null
+
+
+        fun initialize(context: Context){
+            if(INSTANCE == null){
+                val dataStore = PreferenceDataStoreFactory.create {
+                    context.preferencesDataStoreFile("settings")
+                }
+                INSTANCE = PreferencesRepository(dataStore)
+            }
+        }
+
+        fun get(): PreferencesRepository {
+            return INSTANCE ?: throw IllegalStateException(
+                "PreferencesRepository must be initialized"
+            )
+        }
+
+
+    }
+
+
 }
+
+
+
+
