d6469146e9295bf1cbf8bc280ee285c614a6e474
Christian Williams <ciwilliams@cpp.edu>
2023-12-05 01:17:31 -0800

    Startes PollWorker

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.room:room-runtime:2.6.0")
     implementation("androidx.room:room-ktx:2.6.0")
+    implementation ("androidx.datastore:datastore-preferences:1.0.0")
     ksp("androidx.room:room-compiler:2.6.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
--- app/src/main/java/cpp/cs4750/rssfeedreader/repository/PreferencesRepository.kt
+++ app/src/main/java/cpp/cs4750/rssfeedreader/repository/PreferencesRepository.kt
@@ -1,6 +1,20 @@
 package cpp.cs4750.rssfeedreader.repository
 
-class PreferencesRepository {
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import kotlinx.coroutines.flow.Flow
+
+
+class PreferencesRepository (
+    private val dataStore: DataStore<Preferences>
+){
+
+    val storedQuery: Flow<String> = dataStore.data.map{
+        
+    }
+
+
+
     suspend fun setPolling(isPolling: Boolean) {
         TODO()
     }
